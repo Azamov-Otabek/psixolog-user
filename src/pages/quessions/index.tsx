30e02ff9cd76adfc070ba77ba98dda818188054f
Otabek <azamov521o@gmail.com>
2024-09-07 20:34:35 +0500

    completed

--- src/pages/quessions/index.tsx
+++ src/pages/quessions/index.tsx
@@ -98,7 +98,7 @@ function Index() {
     ) : (
       <>
         <ToastContainer />
-        <div className="question">
+        <div className="question" style={{paddingBottom: 150}}>
           <h2 style={{ textAlign: 'center', marginBottom: 40 }}>{poll?.title}</h2>
           {data.length > 0 ? (
             <Form form={form} onFinish={handleSubmit}>
